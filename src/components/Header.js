07bb95f37c3e90ea921f55e51e39084681a62683
Amesese Boateng <ak-boateng>
2023-03-11 10:39:12 +0000

    Both Desktop and Mobile compatility added for Header, NavBar and Slider

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,11 @@ import { FaLock } from 'react-icons/fa';
 const Header = () => {
   return (
     <div className='bg-black  text-white'>
-      <div className='p-5 flex container mx-40 justify-between'>
+      <div className='lg:p-5 p-3 lg:flex flex container lg:mx-40 justify-between'>
       <p>Quick Call : 030 456 1111</p>
-      <ul className=' font-semibold text-1xl flex gap-6'>
-        <a href='google.com'className='inline-flex'><i className='mx-2 my-1'><FaLock /></i>Admin Login</a>
-        <a href='google.com'className='inline-flex'><i className='mx-2 my-1'><FaLock /></i>Donor Login</a>
+      <ul className=' font-semibold lg:text-1xl text-sm flex lg:gap-6 gap-3'>
+        <a href='google.com'className='inline-flex'><i className='lg:mx-2 px-1 my-1'><FaLock /></i>Admin Login</a>
+        <a href='google.com'className='inline-flex'><i className='lg:mx-2 px-1 my-1'><FaLock /></i>Donor Login</a>
         
       </ul>
       </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../images/logo.png'
 import { FaPlusCircle } from 'react-icons/fa';
-import { FaHamburger } from 'react-icons/fa';
+import { FiMenu } from 'react-icons/fi';
 
 
 const NavBar = () => {
-  return (
-      <div className='p-8 flex container mx-40 justify-between'>
-      <img className='w-1/6' src={logo} alt="" />
-      <ul className=' font-semibold pt-2 text-1.5xl flex gap-4'>
-       <a href="google.com">Learn</a>
-       <a href="google.com">Donate</a>
-       <a href="google.com">Eligibilty</a>
-       <a href="google.com">Resources</a>
-       <a href="google.com">Get Involved</a>
-       <a href="google.com">Support Us</a>
-       <a href='google.com'className='inline-flex bg-red-700 text-white pt-1 pr-2 mt-0 rounded'><i className='mx-2 my-1'><FaPlusCircle /></i>DONATE</a>
-      </ul>
+  const [open, setOpen] = useState(false);
 
-      <a href='google.com' className='bg-white p-3 hidden sm:block justify-end'><i><FaHamburger/> </i></a>
+
+  return (
+     <header className='border-b border-gray-300 py-4'>
+       <div className='flex items-center justify-between xl:max-w-8xl xl:mx-auto lg:max-w-full px-[8%] flex-wrap'>
+      <img className='w-1/6' src={logo} alt="" />
+      <FiMenu className='lg:hidden block h-6 w-6 cursor-pointer'onClick= {() => setOpen(!open)}/>
+      
+      {/*Nav Links  */}
+      <nav className={`${open ? 'block' : 'hidden'} lg:flex lg:items-center lg:w-auto w-full `}>
+      <ul className='font-semibold lg:flex block justify-between  text-1.5xl'>
+      <li className='border-b border-gray-300  lg:border-none py-1'>
+        <a className='lg:px-4 lg:py-2   hover:text-red-700' href="google.com">Learn</a>
+        </li>
+        <li className='border-b border-gray-300 lg:border-none py-1'>
+       <a className='lg:px-4 lg:py-2  hover:text-red-700' href="google.com">Donate</a>
+        </li>
+        <li className='border-b border-gray-300 lg:border-none py-1'>
+       <a className='lg:px-4 lg:py-2  hover:text-red-700' href="google.com">Eligibilty</a>
+        </li>
+        <li className='border-b border-gray-300 lg:border-none py-1'>
+       <a className='lg:px-4 lg:py-2 hover:text-red-700' href="google.com">Resources</a>
+        </li>
+        <li className='border-b border-gray-300 lg:border-none py-1'>
+       
+       <a className='lg:px-4 lg:py-2  hover:text-red-700' href="google.com">Get Involved</a>
+        </li>
+        <li className='border-b border-gray-300 lg:border-none py-1'>
+        <a className='lg:px-4 lg:py-2  hover:text-red-700' href="google.com">Support Us</a>
+        </li>
+        <li>
+        <a  href='google.com'className='inline-flex bg-red-700 text-white px-4 py-2 mt-0 rounded w-full hover:w-[+120%] transition delay-150 hover:delay-150 hover:duration-300'><i className='mx-2 my-1'><FaPlusCircle /></i>DONATE</a>
+        </li>
+      </ul>
+      </nav>
+
+      
       </div>
+     </header>
   )
 }
 
